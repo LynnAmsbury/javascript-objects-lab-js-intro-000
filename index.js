387255c7b171c860bb79f266a6859430d9dcc9b9
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-11 22:01:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
   return object
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, obj, { [key]: value })
+}
